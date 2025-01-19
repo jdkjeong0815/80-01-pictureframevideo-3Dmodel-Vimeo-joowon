4403feb85a993612620ce58e9ac8f0d41afcb549
dkjeong0815 <dkjeong0815@gmail.com>
2025-01-19 19:13:49 +0900

    광고  URL

--- sketch.js
+++ sketch.js
@@ -47,123 +47,24 @@ function preload() {
 function setup() {
   minCanvas = min(windowWidth, windowHeight);
   let canva = createCanvas(windowWidth, windowHeight, WEBGL);
-  // 1. Calculate the bounding box of the model
   bbox = calculateBoundingBox(picFrame);
-  // 2. Calculate the scale factor
   scaleFactor = calculateScaleFactor(bbox, width, height);
   minBbox = min(bbox.width, bbox.height);
 
-   noScroll(); // 스크롤 금지. 스크롤바 생기는 것 방지
-  // console.log('Bounding Box:', bbox, minBbox);
-  // console.log('Scale Factor:', scaleFactor);
+  noScroll();
+  setupText();
+  setupVimeo(minBbox, scaleFactor, videoSize);
 
-  // 텍스트 
-  // 고밀도 렌더링
-  pixelDensity(3); 
-  // 텍스처 필터링 설정
-  textureWrap(REPEAT);
-  textureMode(NORMAL);
-  noStroke(); // 선 없음
-
-  // 2D 캔버스 생성 후 텍스트 사용시 절차
-  // 0) createGraphics는 push, pop내에 translate()에 의해 좌표 이동 함!!!
-  // 1) createGraphics 크기를 캔버스에 비례하여 지정
-  // 2) createGraphics 의 좌표 중심은 top, left가 0, 0 원점 기준임
-  // 3) 그래서 text(string, x, y)에서 x, y의 원점은 default로 createGraphics 따라 감 
-  // 4) 따라서 text(string, x, y)에서 x, y를 pg.width/2, pg.height/2로 해야 함
-  // 5) 그러므로 textAlign() 함수에서 사용되는 파라미터는 text 중심 기준으로 설정 됨을 주의 
-  // 6) 폰트에 따라 다른 높이 위치를 미세 조정으로 해야 함.
-  // 7) Align 파라미터: 기본값은 (LEFT, BASELINE) 
-  //    좌우 정렬: LEFT, CENTER, RIGHT / 높이 정렬: TOP, CENTER, BOTTOM, BASELINE
-  // 8) createGraphics로 그린 후 plane에 텍스쳐로 레스터라이즈 하므로 2개의 크기를 맞춰야 정상.
-  //    다르면 찌그러지거나 강제로 당겨진 듯하게 표시 됨.
-
-  let scaleFontFactor = map(width / height, 0.5, 2, 0.015, 0.02, true); // 화면 비율에 따라 폰트 비율 조정
-  let baseFontSize = min(width, height) * scaleFontFactor;
-  let aspectRatio = innerWidth*innerHeight/1;
-  console.log("font size: ", baseFontSize, aspectRatio);
-
-  pgw = bbox.width*scaleFactor*0.8;  //좌우 여백 고려. 3d 모델 설정과 동일하게 가야 함
-  pgh = baseFontSize+15;  // 베이스라인 고려
-  
-  pg = createGraphics(pgw, pgh);
-  //pg.background(100);  // 배경 색상
-  pg.textFont(myFont);
-  pg.textSize(baseFontSize);
-  pg.fill(255);  // 글자 색상
-  pg.textAlign(CENTER, CENTER);  // BASELINE 폰트 얼라인먼트. text()의 x, y 중심 기준으로 배치 주의
-  pg.text("LOST IN WHITE,  by 김주원 작가", pgw/2, pgh/2);  // 폰트 종류에 따라 textAlign과 함께 미세 조정 필요
-  // pg.textSize(28);
-  // pg.fill(255, 255, 0);  // 글자 색상
-  // pg.text("모든 가정에 건강과 행복이 깃들기를 기원드립니다...!", pgw/2, pgh/2+60);
-
-
-  // 비디오 재생 : iFrame 방식
-  let iframe = createDiv(`
-    <iframe 
-      src="https://player.vimeo.com/video/1048151282?muted=1&autoplay=1&quality=1080p?controls=0&dnt=1"  
-      frameborder="0" 
-      allow="autoplay;">
-    </iframe>
-  `);
-
-  // iFrame 크기 설정
-  
-  let iframeWidth =  minBbox*scaleFactor*videoSize*1.3; 
-  let iframeHeight =  minBbox*scaleFactor*videoSize*0.963; 
-
-  let iframeElement = iframe.elt.querySelector('iframe');
-  iframeElement.style.width = `${iframeWidth}px`;
-  iframeElement.style.height = `${iframeHeight}px`;
-
-  // iFrame 위치 설정 - 중앙 배치
-  iframe.position((windowWidth - iframeWidth) / 2, (windowHeight - iframeHeight) / 2);
-
-  // Vimeo Player API 초기화
-  player = new Vimeo.Player(iframe.elt.querySelector('iframe'));
-
-  // Loop 설정
-  player.setLoop(true).then(() => {
-    console.log("Loop 활성화");
-  }).catch((error) => {
-    console.error("Loop 설정 실패:", error);
-  });
-
-}
-// setup() END
-
+  // Ensure WebGL context is properly set up
+  // canva.parent('canvas-container');
+  noStroke(); // Ensure no stroke for the 3D model
+}  // setup() END
 
 function draw() {
-  //orbitControl(); // 마우스로 회전 및 확대/축소 가능
-  //background(bgImg); // 어두운 배경
-  background(180); // 40
-
-  // 360도 주변광 설정
-  ambientLight(160);
-
-  // 특정 방향에서 오는 조명 추가
-  let dirLight = 255;
-  let pLight = 300;
-  directionalLight(dirLight, dirLight, dirLight, 1, 1, -1);
-  pointLight(pLight, pLight, pLight, 1, 1, 1);
-
-// 3d 모델 picture frame 로드
-  push();
-    //rotateX(PI/2);
-    scale(scaleFactor*0.87, scaleFactor*1);  // 양 옆으로 약간의 공간이 있도록 크기 줄임(0.9)
-    model(picFrame); 
-    //console.log(scale(scaleFactor*0.9, scaleFactor*1));
-  pop();
-
-  
-  push();
-    let aa = (bbox.height*scaleFactor)*0.46;
-    //console.log("aa: ", aa, height, bbox.height*scaleFactor);
-    translate(0, aa, 110);  //minBbox*scaleFactor*videoSize*0.7  / bbox.height*1.4
-    texture(pg);
-    plane(pgw, pgh); // 텍스트가 그려진 평면
-  pop();
-
+  background(180);  // 배경색. 비디오 분위기와 맞추기 위해 회색으로 설정
+  setupLighting();
+  drawModel(picFrame, scaleFactor);  // Updated to use modelObj parameter name
+  drawText(bbox, scaleFactor, pgw, pgh, pg);
 }  // draw() END
 
 function titleText() {
@@ -175,51 +76,6 @@ function titleText() {
     texture(pg);
     plane(pgw, pgh); // 텍스트가 그려진 평면
   pop();
-}
-
-function calculateBoundingBox(model) {
-  let vertices = model.vertices;
-  if (!vertices) {
-    console.error('Model does not contain vertices.');
-    return null;
-  }
-
-  let Infinity = 999999999;
-  let minX = Infinity, maxX = -Infinity;
-  let minY = Infinity, maxY = -Infinity;
-  let minZ = Infinity, maxZ = -Infinity;
-
-  for (let i = 0; i < vertices.length; i += 3) {
-    let x = vertices[i];
-    let y = vertices[i + 1];
-    let z = vertices[i + 2];
-
-    // y, y, z는 객체이므로 x.x, y.y, z.z로 접근
-    if (x.x < minX) minX = x.x;
-    if (x.x > maxX) maxX = x.x;
-
-    if (y.y < minY) minY = y.y;
-    if (y.y > maxY) maxY = y.y;
-
-    if (z.z < minZ) minZ = z.z;
-    if (z.z > maxZ) maxZ = z.z;
-    //console.log("xyz: ", i, x, y, z);
-    //console.log("minmax: ", minX, maxX, minY, maxY, minZ, maxZ);
-  }
-
-  return {
-    width: maxX - minX,
-    height: maxY - minY,
-    depth: maxZ - minZ,
-    minX, maxX, minY, maxY, minZ, maxZ
-  };
-}
-
-function calculateScaleFactor(bbox, canvasWidth, canvasHeight) {
-  let modelMaxDimension = max(bbox.width, bbox.height, bbox.depth);  // depth 무시. 
-  let canvasMinDimension = min(canvasWidth, canvasHeight);
-  //console.log('Model Max Dimension:', modelMaxDimension, canvasMinDimension);
-  return canvasMinDimension / modelMaxDimension;
 }
 
 function noScroll() {
---
+++ utils.js
@@ -0,0 +1,112 @@
+function calculateBoundingBox(model) {
+  let vertices = model.vertices;
+  if (!vertices) {
+    console.error('Model does not contain vertices.');
+    return null;
+  }
+
+  let Infinity = 999999999;
+  let minX = Infinity, maxX = -Infinity;
+  let minY = Infinity, maxY = -Infinity;
+  let minZ = Infinity, maxZ = -Infinity;
+
+  for (let i = 0; i < vertices.length; i += 3) {
+    let x = vertices[i];
+    let y = vertices[i + 1];
+    let z = vertices[i + 2];
+
+    if (x.x < minX) minX = x.x;
+    if (x.x > maxX) maxX = x.x;
+
+    if (y.y < minY) minY = y.y;
+    if (y.y > maxY) maxY = y.y;
+
+    if (z.z < minZ) minZ = z.z;
+    if (z.z > maxZ) maxZ = z.z;
+  }
+
+  return {
+    width: maxX - minX,
+    height: maxY - minY,
+    depth: maxZ - minZ,
+    minX, maxX, minY, maxY, minZ, maxZ
+  };
+}
+
+function calculateScaleFactor(bbox, canvasWidth, canvasHeight) {
+  let modelMaxDimension = max(bbox.width, bbox.height, bbox.depth);
+  let canvasMinDimension = min(canvasWidth, canvasHeight);
+  return canvasMinDimension / modelMaxDimension;
+}
+
+function setupVimeo(minBbox, scaleFactor, videoSize) {
+  let iframe = createDiv(`
+    <iframe 
+      src="https://player.vimeo.com/video/1048268331?muted=1&autoplay=1&quality=1080p?controls=0&dnt=1"  
+      frameborder="0" 
+      allow="autoplay;">
+    </iframe>
+  `);
+  
+ //   https://player.vimeo.com/video/1048268331  [Commercial] LG(엘지) OLED_The Black 4K HDR
+ //   https://player.vimeo.com/video/1048151282 김주원 작가
+
+ 
+
+  let iframeWidth = minBbox * scaleFactor * videoSize * 1.3; 
+  let iframeHeight = minBbox * scaleFactor * videoSize * 0.963; 
+
+  let iframeElement = iframe.elt.querySelector('iframe');
+
+  iframeElement.style.width = `${iframeWidth}px`;
+  iframeElement.style.height = `${iframeHeight}px`;
+
+  iframe.position((windowWidth - iframeWidth) / 2 - 2, (windowHeight - iframeHeight) / 2);
+
+  let player = new Vimeo.Player(iframe.elt.querySelector('iframe'));
+
+  player.setLoop(true).then(() => {
+    console.log("Loop 활성화");
+  }).catch((error) => {
+    console.error("Loop 설정 실패:", error);
+  });
+}
+
+function setupLighting() {
+  ambientLight(160);
+  directionalLight(255, 255, 255, 1, 1, -1);
+  pointLight(300, 300, 300, 1, 1, 1);
+}
+
+function drawModel(modelObj, scaleFactor) {
+  push();
+  scale(scaleFactor * 0.87, scaleFactor * 1);
+  model(modelObj); 
+  pop();
+}
+
+function setupText() {
+  let scaleFontFactor = map(width / height, 0.5, 2, 0.015, 0.02, true);
+  let baseFontSize = min(width, height) * scaleFontFactor;
+  let aspectRatio = innerWidth * innerHeight / 1;
+  console.log("font size: ", baseFontSize, aspectRatio);
+
+  pgw = bbox.width * scaleFactor * 0.8;
+  pgh = baseFontSize + 15;
+  
+  pg = createGraphics(pgw, pgh);
+  pg.textFont(myFont);
+  pg.textSize(baseFontSize);
+  pg.fill(255);
+  pg.textAlign(CENTER, CENTER);
+  pg.text("LOST IN WHITE,  by 김주원 작가", pgw / 2, pgh / 2);
+}
+
+function drawText(bbox, scaleFactor, pgw, pgh, pg) {
+  push();
+  let aa = (bbox.height * scaleFactor) * 0.46;
+  translate(0, aa, 110);
+  texture(pg);
+  plane(pgw, pgh);
+  pop();
+}
